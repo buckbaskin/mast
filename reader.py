7e2c9fce0283491e1a0e57a06200d02787ffb884
Buck Baskin <mobile.wbaskin@gmail.com>
2023-05-11 22:46:55 -0700

    Download 10K

--- reader.py
+++ reader.py
@@ -1,6 +1,8 @@
+import os
 from mastodon import Mastodon
 from pprint import pprint
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 mastodon = Mastodon(
     api_base_url="https://fosstodon.org", user_agent="formakio", ratelimit_method="wait"
@@ -14,17 +16,17 @@ mastodon = Mastodon(
 def cli_render(html_str, raw=False, rendered=True, links=False):
     soup = BeautifulSoup(html_str, features="html.parser")
     if raw:
-        print(html_str)
+        yield (html_str)
     if rendered:
-        print(soup.get_text())
+        yield (soup.get_text())
     if links:
         collection = soup.find_all("a")
         if len(collection) > 0:
-            print("Links")
+            yield ("Links")
             for link in collection:
                 text = link.get_text()
                 url = link["href"]
-                print(f" - {text} -> {url}")
+                yield (f" - {text} -> {url}")
 
 
 def toot_stream(m, *args, **kwargs):
@@ -35,12 +37,23 @@ def toot_stream(m, *args, **kwargs):
         page = m.fetch_next(page._pagination_next)
 
 
-for idx, toot in enumerate(toot_stream(mastodon, timeline="local")):
-    if idx > 24:
+for idx, toot in tqdm(enumerate(toot_stream(mastodon, timeline="local"))):
+    if idx >= 10000:
         break
 
-    print()
-    print(toot["id"])
-    print(" ".join(toot["account"]["acct"].split()))
-    print("Content")
-    cli_render(toot["content"])
+    id_ = str(toot["id"])
+    try:
+        open(os.path.join("data", id_))
+    except FileNotFoundError:
+        with open(os.path.join("data", id_), "w") as f:
+            flattened_username = " ".join(toot["account"]["acct"].split())
+            f.write(
+                "\n".join(
+                    [
+                        str(id_),
+                        flattened_username,
+                        "Content",
+                        "\n".join(cli_render(toot["content"])),
+                    ]
+                )
+            )
