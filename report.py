2e666dd202363fad1c2dcf5807d3789c786d09f3
Buck Baskin <mobile.wbaskin@gmail.com>
2023-05-19 23:23:09 -0700

    Add report functionality

---
+++ report.py
@@ -0,0 +1,39 @@
+"""
+Report: Show authors whose content you like, ordered by the amount of their content that you like
+"""
+import sqlite3
+from itertools import chain
+
+# Database Setup
+con = sqlite3.connect("data/db.db")
+cur = con.cursor()
+
+existing_tables = cur.execute("SELECT name from sqlite_master")
+if "ratings" not in chain.from_iterable(existing_tables.fetchall()):
+    raise ValueError('ratings table not in data/db.db')
+
+
+new_con = sqlite3.connect("data/db.db")
+new_cur = new_con.cursor()
+
+(row_count,) = new_cur.execute("select count(id) from ratings").fetchone()
+
+print("Number of Ratings So Far:", row_count)
+
+(row_count,) = new_cur.execute("select count(id) from ratings where score > 0").fetchone()
+
+print('Number of positive ratings', row_count)
+
+####
+padding = len(str(row_count))
+
+for idx, row in enumerate(
+    new_cur.execute(
+        "SELECT author, count(author), max(content) FROM ratings WHERE score > 0 GROUP BY author ORDER BY count(author) DESC"
+    )
+):
+    author, positive_cases, example_content  = row
+
+    print(str(positive_cases).ljust(padding), author)
+    print('   -', example_content)
+
