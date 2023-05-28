ed9d3a9ae72802816dc29686d670d4a6cb474109
Buck Baskin <mobile.wbaskin@gmail.com>
2023-05-28 00:59:23 -0700

    Flatten core/explore.py

--- core/explore.py
+++ core/explore.py
@@ -9,6 +9,54 @@ from config import API_BASE_URL
 from core.utils import render_author
 
 
+def existing_ratings(*, new_cur):
+    for (id_,) in new_cur.execute(
+        "SELECT id from ratings ORDER BY 75 * id + 74 % 65537"
+    ):
+        yield id_
+
+
+def toot_explore(max_single_explore, *, new_cur):
+    hard_cap = max_single_explore * 2
+    existing = set(existing_ratings(new_cur=new_cur))
+
+    count = 0
+
+    for row in new_cur.execute(
+        "SELECT id, author, content, (75 * id + 74) % 65537 FROM toots ORDER BY (75 * id + 74) % 65537"
+    ):
+        id_, author, content, hash_ = row
+
+        host = API_BASE_URL
+
+        if id_ in existing:
+            continue
+
+        count += 1
+        if count > max_single_explore or count > hard_cap:
+            break
+
+        print(
+            "\n=== Content %3d / %3d ===\n%s\n    %s"
+            % (count, max_single_explore, content, render_author(author, host))
+        )
+
+        result = input("- dislike + like else skip ")
+
+        if result in ["-", "=", "+", "0"]:
+            if result == "-":
+                yield (id_, author, content, -1)
+            elif result == "=" or result == "+":
+                print("...   + bonus!")
+                max_single_explore += 1
+                yield (id_, author, content, 1)
+            elif result == "0":
+                yield (id_, author, content, 0)
+
+        else:
+            pass
+
+
 def explore_impl(toots_limit):
     # Database Setup
     con = sqlite3.connect("data/db.db")
@@ -24,53 +72,7 @@ def explore_impl(toots_limit):
 
     (row_count,) = new_cur.execute("select count(id) from ratings").fetchone()
 
-    def existing_ratings():
-        for (id_,) in new_cur.execute(
-            "SELECT id from ratings ORDER BY 75 * id + 74 % 65537"
-        ):
-            yield id_
-
-    def toot_explore(max_single_explore):
-        hard_cap = max_single_explore * 2
-        existing = set(existing_ratings())
-
-        count = 0
-
-        for row in new_cur.execute(
-            "SELECT id, author, content, (75 * id + 74) % 65537 FROM toots ORDER BY (75 * id + 74) % 65537"
-        ):
-            id_, author, content, hash_ = row
-
-            host = API_BASE_URL
-
-            if id_ in existing:
-                continue
-
-            count += 1
-            if count > max_single_explore or count > hard_cap:
-                break
-
-            print(
-                "\n=== Content %3d / %3d ===\n%s\n    %s"
-                % (count, max_single_explore, content, render_author(author, host))
-            )
-
-            result = input("- dislike + like else skip ")
-
-            if result in ["-", "=", "+", "0"]:
-                if result == "-":
-                    yield (id_, author, content, -1)
-                elif result == "=" or result == "+":
-                    print("...   + bonus!")
-                    max_single_explore += 1
-                    yield (id_, author, content, 1)
-                elif result == "0":
-                    yield (id_, author, content, 0)
-
-            else:
-                pass
-
-    to_write = list(toot_explore(toots_limit))
+    to_write = list(toot_explore(toots_limit, new_cur=new_cur))
 
     new_cur.executemany("INSERT INTO ratings VALUES(?, ?, ?, ?)", to_write)
     new_con.commit()
