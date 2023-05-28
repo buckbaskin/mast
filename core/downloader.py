f359607e2d5c6bc0e005254141ddeb69e4b2c6b9
Buck Baskin <mobile.wbaskin@gmail.com>
2023-05-28 00:55:23 -0700

    Flatten downloader.py

--- core/downloader.py
+++ core/downloader.py
@@ -9,6 +9,59 @@ from tqdm import tqdm
 from config import API_BASE_URL
 
 
+def render_paragraphs(soup):
+    for p in soup.find_all("p"):
+        yield p.get_text()
+
+
+def cli_render(html_str, raw=False, rendered=True, links=False):
+    if raw:
+        yield (html_str)
+
+    soup = BeautifulSoup(html_str, features="html.parser")
+    if rendered:
+        for line_break in soup.find_all("br"):
+            line_break.replaceWith(" ")
+
+        text = " ".join(render_paragraphs(soup))
+        yield text
+
+    if links:
+        collection = soup.find_all("a")
+        if len(collection) > 0:
+            yield ("Links")
+            for link in collection:
+                text = link.get_text()
+                url = link["href"]
+                yield (f" - {text} -> {url}")
+
+
+def toot_stream(m, *args, **kwargs):
+    page = m.timeline(*args, **kwargs)
+
+    while True:
+        yield from page
+        page = m.fetch_next(page._pagination_next)
+
+
+def as_db_tuples(toots_limit, *, mastodon, min_id_in_db):
+    skipped = 0
+
+    for idx, toot in tqdm(
+        enumerate(toot_stream(mastodon, timeline="local", max_id=min_id_in_db))
+    ):
+        if idx >= toots_limit + skipped:
+            break
+
+        flattened_username = " ".join(toot["account"]["acct"].split())
+        content = "\n".join(cli_render(toot["content"]))
+
+        if len(content) > 0:
+            yield toot["id"], flattened_username, content
+        else:
+            skipped += 1
+
+
 def download_impl(parsed_args):
     toots_limit = parsed_args.toots
 
@@ -30,59 +83,13 @@ def download_impl(parsed_args):
         ratelimit_pacefactor=0.95,
     )
 
-    def render_paragraphs(soup):
-        for p in soup.find_all("p"):
-            yield p.get_text()
-
-    def cli_render(html_str, raw=False, rendered=True, links=False):
-        if raw:
-            yield (html_str)
-
-        soup = BeautifulSoup(html_str, features="html.parser")
-        if rendered:
-            for line_break in soup.find_all("br"):
-                line_break.replaceWith(" ")
-
-            text = " ".join(render_paragraphs(soup))
-            yield text
-
-        if links:
-            collection = soup.find_all("a")
-            if len(collection) > 0:
-                yield ("Links")
-                for link in collection:
-                    text = link.get_text()
-                    url = link["href"]
-                    yield (f" - {text} -> {url}")
-
-    def toot_stream(m, *args, **kwargs):
-        page = m.timeline(*args, **kwargs)
-
-        while True:
-            yield from page
-            page = m.fetch_next(page._pagination_next)
-
-    def as_db_tuples(toots_limit):
-        skipped = 0
-
-        for idx, toot in tqdm(
-            enumerate(toot_stream(mastodon, timeline="local", max_id=min_id_in_db))
-        ):
-            if idx >= toots_limit + skipped:
-                break
-
-            flattened_username = " ".join(toot["account"]["acct"].split())
-            content = "\n".join(cli_render(toot["content"]))
-
-            if len(content) > 0:
-                yield toot["id"], flattened_username, content
-            else:
-                skipped += 1
-
     (pre_row_count,) = cur.execute("select count(id) from toots").fetchone()
 
     try:
-        cur.executemany("INSERT INTO toots VALUES(?, ?, ?)", as_db_tuples(toots_limit))
+        cur.executemany(
+            "INSERT INTO toots VALUES(?, ?, ?)",
+            as_db_tuples(toots_limit, mastodon=mastodon, min_id_in_db=min_id_in_db),
+        )
         con.commit()
     finally:
         con.close()
